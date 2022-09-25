2f48a8b69ce14d6ec0ffa3c2cf3214d4f65ccc3a
dmitriy <master71512@mail.ru>
2022-09-25 20:25:01 +0300

    27 solved

---
+++ 27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (a>0)
+{
+    int k = a % 10;
+    sum += k;
+    a /= 10;
+}
+Console.WriteLine("сумма цифр равна " + sum);
